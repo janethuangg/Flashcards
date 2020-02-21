a0c4b936e11465bc1bd2060e3e9fa2d3a8326645
janethuangg <jh6248@nyu.edu>
2020-02-21 12:07:32 -0500

    completed base functionality

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answer: UILabel!
+    @IBOutlet weak var question: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapCard(_ sender: Any) {
+        if question.isHidden == true {
+            question.isHidden = false
+        } else {
+            question.isHidden = true
+        }
+    }
 }
 
