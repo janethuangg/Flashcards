ab1c226b7b53a2cdb1f7b27b25a917c71ef62761
janethuangg <jh6248@nyu.edu>
2020-03-12 22:55:18 -0400

    significant progress -- card creation + storage

---
+++ CreationViewController.swift
@@ -0,0 +1,69 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Janet Huang on 3/11/20.
+//  Copyright © 2020 Janet Huang. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var opt1TextField: UITextField!
+    @IBOutlet weak var opt2TextField: UITextField!
+    @IBOutlet weak var opt3TextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    var initialQuestion: String?
+    var initialAnswer: String?
+    var initialOpt1: String?
+    var initialOpt2: String?
+    var initialOpt3: String?
+    
+    var flashcardsController: ViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionTextField.text = initialQuestion
+        answerTextField.text = initialAnswer
+        opt1TextField.text = initialOpt1
+        opt2TextField.text = initialOpt2
+        opt3TextField.text = initialOpt3
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        let questionText = questionTextField.text
+        let opt1Text = opt1TextField.text
+        let opt2Text = opt2TextField.text
+        let opt3Text = opt3TextField.text
+        let answerText = answerTextField.text
+        
+        if (questionText == nil || answerText == nil || questionText!.isEmpty || answerText!.isEmpty){
+            let alert = UIAlertController(title: "Missing text", message: "Fill out all fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+            }))
+            present(alert, animated: true)
+        } else {
+            flashcardsController.updateCard(newQuestion: questionText!, newOpt1: opt1Text!, newOpt2: opt2Text!, newOpt3: opt3Text!, newAnswer: answerText!)
+            dismiss(animated: true)
+        }
+    }
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String
+    var opt1: String
+    var opt2: String
+    var opt3: String
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var answer: UILabel!
@@ -16,9 +24,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var opt1: UIButton!
     @IBOutlet weak var opt2: UIButton!
     @IBOutlet weak var opt3: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        readFlashcardsFromDisk()
+        
+        if flashcards.count == 0{
+              updateCard(newQuestion: "What's the capital of Brazil?", newOpt1: "Brasilia", newOpt2: "Rio de Janeiro", newOpt3: "Sao Paulo", newAnswer: "Brasilia")
+        } else {
+            updateLabels()
+            updateNextPrevButtons()
+        }
+        
+        format()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardsController = self
+        
+        if segue.identifier == "EditSegue"{
+            creationController.initialQuestion = question.text
+            creationController.initialAnswer = answer.text
+            creationController.initialOpt1 = opt1.currentTitle
+            creationController.initialOpt2 = opt2.currentTitle
+            creationController.initialOpt3 = opt3.currentTitle
+        }
+    }
+
+    @IBAction func didTapCard(_ sender: Any) {
+        if question.isHidden == true {
+            question.isHidden = false
+        } else {
+            question.isHidden = true
+        }
+    }
+    
+    func format(){
         card.layer.cornerRadius = 20.0
         card.layer.shadowRadius = 15.0
         card.layer.shadowOpacity = 0.2
@@ -31,47 +80,162 @@ class ViewController: UIViewController {
         opt1.layer.borderWidth = 3.0
         opt1.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         opt1.layer.cornerRadius = 20.0
+        opt1.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        opt1.setTitleColor(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), for: [])
+        
         opt2.layer.borderWidth = 3.0
         opt2.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         opt2.layer.cornerRadius = 20.0
+        opt2.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        opt2.setTitleColor(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), for: [])
+        
         opt3.layer.borderWidth = 3.0
         opt3.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         opt3.layer.cornerRadius = 20.0
+        opt3.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        opt3.setTitleColor(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), for: [])
+        
+        question.isHidden = false
     }
-
-    @IBAction func didTapCard(_ sender: Any) {
-        if question.isHidden == true {
-            question.isHidden = false
+    
+    func updateCard(newQuestion: String, newOpt1: String, newOpt2: String, newOpt3: String, newAnswer:String) {
+        let flashcard = Flashcard(question: newQuestion, answer: newAnswer, opt1: newOpt1, opt2: newOpt2, opt3: newOpt3)
+        
+        question.text = flashcard.question
+        answer.text = flashcard.answer
+        opt1.setTitle(flashcard.opt1, for: .normal)
+        opt2.setTitle(flashcard.opt2, for: .normal)
+        opt3.setTitle(flashcard.opt3, for: .normal)
+        
+        format()
+        
+        flashcards.append(flashcard)
+        print("added new flashcard")
+        print("we now have \(flashcards.count) flashcards")
+        currentIndex = flashcards.count-1
+        print("our current index is \(currentIndex)")
+        
+        saveFlashcardsToDisk()
+        updateNextPrevButtons()
+        updateLabels()
+    }
+    
+    func updateNextPrevButtons(){
+        if currentIndex == flashcards.count-1{
+            print("end")
+            nextButton.isEnabled = false
         } else {
-            question.isHidden = true
+            nextButton.isEnabled = true
+        }
+        
+        if currentIndex == 0{
+            prevButton.isEnabled = false
+        } else {
+            prevButton.isEnabled = true
         }
     }
     
     @IBAction func didTapOpt1(_ sender: Any) {
-        opt1.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt1.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt1.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+        if answer.text == opt1.currentTitle{
+            opt1.layer.backgroundColor = #colorLiteral(red: 0.2393075824, green: 0.6728859544, blue: 0.9679804444, alpha: 1)
+            opt1.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: [])
+            
+            opt2.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt2.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt2.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+            
+            opt3.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt3.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt3.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+            
+            question.isHidden = true
+        } else {
+            opt1.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt1.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt1.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+        }
     }
     
     @IBAction func didTapOpt2(_ sender: Any) {
-        opt1.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt1.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt1.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
-        
-        opt2.layer.backgroundColor = #colorLiteral(red: 0.2393075824, green: 0.6728859544, blue: 0.9679804444, alpha: 1)
-        opt2.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: [])
-        
-        opt3.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt3.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt3.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
-        
-        question.isHidden = true
+        if answer.text == opt2.currentTitle{
+            opt2.layer.backgroundColor = #colorLiteral(red: 0.2393075824, green: 0.6728859544, blue: 0.9679804444, alpha: 1)
+            opt2.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: [])
+            
+            opt1.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt1.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt1.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+            
+            opt3.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt3.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt3.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+            
+            question.isHidden = true
+        } else {
+            opt2.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt2.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt2.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+        }
     }
     
     @IBAction func didTapOpt3(_ sender: Any) {
-        opt3.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt3.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        opt3.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+        if answer.text == opt3.currentTitle{
+            opt3.layer.backgroundColor = #colorLiteral(red: 0.2393075824, green: 0.6728859544, blue: 0.9679804444, alpha: 1)
+            opt3.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: [])
+            
+            opt1.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt1.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt1.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+            
+            opt2.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt2.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt2.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+            
+            question.isHidden = true
+        } else {
+            opt3.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt3.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            opt3.setTitleColor(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: [])
+        }
+    }
+    
+    @IBAction func didTapNext(_ sender: Any) {
+        currentIndex = currentIndex + 1
+        updateLabels()
+        updateNextPrevButtons()
+        format()
+    }
+    
+    @IBAction func didTapPrev(_ sender: Any) {
+        currentIndex = currentIndex - 1
+        updateLabels()
+        updateNextPrevButtons()
+        format()
+    }
+    
+    func updateLabels() {
+        let currentFlashcard = flashcards[currentIndex]
+        
+        question.text = currentFlashcard.question
+        answer.text = currentFlashcard.answer
+        opt1.setTitle(currentFlashcard.opt1, for: .normal)
+        opt2.setTitle(currentFlashcard.opt2, for: .normal)
+        opt3.setTitle(currentFlashcard.opt3, for: .normal)
+    }
+    
+    func saveFlashcardsToDisk(){
+        let dictionaryArray = flashcards.map{ (card) -> [String: String] in return ["question" : card.question, "opt1": card.opt1, "opt2": card.opt2, "opt3": card.opt3, "answer": card.answer]
+        }
+        
+        UserDefaults.standard.set(dictionaryArray, forKey: "flashcards")
+    }
+    
+    func readFlashcardsFromDisk(){
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as? [[String:String]]{
+            let savedCards = dictionaryArray.map{ dictionary -> Flashcard in return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!, opt1: dictionary["opt1"]!, opt2: dictionary["opt2"]!, opt3: dictionary["opt3"]!)
+            }
+            flashcards.append(contentsOf: savedCards)
+        }
     }
 }
+
 
